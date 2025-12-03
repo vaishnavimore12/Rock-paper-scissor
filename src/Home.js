29f1c203e55fa3541ec0dfe683c14bc80d1a9962
Vaishu <Vaishu@gmail.com>
2025-12-03 23:12:40 +0530

    Updated files

--- src/Home.js
+++ src/Home.js
@@ -11,11 +11,10 @@ import brock from "./images/black_rock.png";
 import bscissor from "./images/black_scissor.png";
 import vs3 from "./images/vs3.png";
 
-
 export function Home() {
-  const buttonImg = [hello,fist,peace];
-  const userChoice = [paper,rock,scissor];
-  const botChoice = [bpaper,brock,bscissor];
+  const buttonImg = [hello, fist, peace];
+  const userChoice = [paper, rock, scissor];
+  const botChoice = [bpaper, brock, bscissor];
 
   const [userImg, setUserImg] = useState(rock);
   const [botImg, setBotImg] = useState(brock);
@@ -23,7 +22,9 @@ export function Home() {
   const [result, setResult] = useState("Make a move");
 
   function determineResult(userImg, botImg) {
-    if (userImg === botImg.replace('black_','')) {
+    if((userImg === rock && botImg === brock) ||
+    (userImg === scissor && botImg === bscissor) ||
+    (userImg === paper && botImg === bpaper)){
       return "It's Tie";
     } else if (
       (userImg === rock && botImg === bscissor) ||
@@ -37,58 +38,49 @@ export function Home() {
   }
 
   const handleClick = (userChoice) => {
-   
-   
-     setUserImg(rock);
-   
+    setUserImg(rock);
+
     setBotImg(brock);
     setAnimate(true);
     setResult("");
-    setTimeout(() =>{setAnimate(false);
+    setTimeout(() => {
+      setAnimate(false);
       setUserImg(userChoice);
-    
-    const randomBotImg =
-    botChoice[Math.floor(Math.random() * botChoice.length)];
-    
-    setBotImg(randomBotImg);
-      
 
+      const randomBotImg =
+        botChoice[Math.floor(Math.random() * botChoice.length)];
 
-    
-      const gameResult =determineResult(userChoice,randomBotImg);
-    setResult(gameResult);
-    
-    },300);
-    
+      setBotImg(randomBotImg);
 
+      const gameResult = determineResult(userChoice, randomBotImg);
+      setResult(gameResult);
+    }, 300);
   };
   return (
     <div className="container">
       <div className="con1">
-         
         <div className="user_img">
-       <label>You</label>
+          <label>You</label>
           <img src={userImg} alt="" className={animate ? "shake" : ""} />
         </div>
         <div className="vs">
-          <img src={vs3} alt=""/>
+          <img src={vs3} alt="" />
         </div>
-        
+
         <div className="com_img">
-        <label>Bot</label>
+          <label>Bot</label>
           <img src={botImg} alt="" className={animate ? "shake" : ""} />
         </div>
 
         <div className="result">{result}</div>
-        </div>
-        <div className="option">
-          {buttonImg.map((buttonImage, index) => (
-            <button key={index} onClick={() => handleClick(userChoice[index])}>
-              <img src={buttonImage} alt="" />
-            </button>
-          ))}
-        </div>
-     
+      </div>
+      <div className="option">
+        {buttonImg.map((buttonImage, index) => (
+          <button key={index} onClick={() => handleClick(userChoice[index])}>
+            <img src={buttonImage} alt="" />
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
